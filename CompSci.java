66af93db38e7875401956580d1fdcebd580a2ae3
TUFFSHI <27lovea@smtexas.org>
2025-05-11 19:12:58 -0500

    Update CompSci.java

--- CompSci.java
+++ CompSci.java
@@ -24,7 +24,7 @@ private String subject
     // Set the options variable to the list of answer options provided
     // Set the correctAnswerIndex variable to the index of the correct answer
     // Set the subject variable to the subject of the question
-    public Question(String questionText, String[] options, int correctAnswerIndex,      String subject)
+    public Question(String questionText, String[] options, int correctAnswerIndex, String subject)
     {
         //set questionText to text
         this.questionText = questionText;
@@ -101,7 +101,7 @@ class Player
     //Properties:
     //name: String
     public String name;
-    //characterClass: String  // "History Mage", "Math Warrior", or "Code                 Wizard"
+    //characterClass: String  // "History Mage", "Math Warrior", or "Code Wizard"
     public String characterClass;
     //health: Integer
     public int health;
@@ -119,106 +119,195 @@ class Player
     private ArrayList<String> powerUps;
 
     //Constructor:
-        Set name to given name
-        Set characterClass to given characterClass
-        Set health to 100
-        Set maxHealth to 100
-        Set attack to 10
-        Set level to 1
-        Set xp to 0
-        Set xpToNextLevel to 5
-        Initialize powerUps as an empty list
+    public Player(String name, String characterClass)
+    {
+        //Set name to given name
+        this.name = name;
+        //Set characterClass to given characterClass
+        this.characterClass = characterClass;
+        //Set health to 100
+        this.health = 100;
+        //Set maxHealth to 100
+        this.maxHealth = 100;
+        //Set attack to 10
+        this.attack = 10;
+        //Set level to 1
+        this.level = 1;
+        //Set xp to 0
+        this.xp = 0;
+        //Set xpToNextLevel to 5
+        this.xpToNextLevel = 5;
+        //Initialize powerUps as an empty list
+        this.powerUps = new ArrayList<String>();
+    }
 
-    Method attackEnemy(enemy):
-        Calculate damage (equal to attack)
-        Print "[player name] attacks for [damage] damage!"
-        Call enemy.takeDamage(damage)
+    //Method attackEnemy(enemy):
+    public void attackEnemy(Enemy enemy)
+    {
+        //Calculate damage (equal to attack)
+        int damage = attack;
+        //Print "[player name] attacks for [damage] damage!"
+        System.out.println(name + " attacks for " + damage + " damage!");
+        //Call enemy.takeDamage(damage)
+        enemy.takeDamage(damage);
+    }
 
-    Method takeDamage(amount):
-        Subtract amount from health
-        If health is less than 0, set health to 0
-        Print "[player name] has [health] health remaining."
+    //Method takeDamage(amount):
+    public void takeDamage(int amount)
+    {
+        //Subtract amount from health
+        health -= amount;
+        //If health is less than 0, set health to 0
+        if (health < 0)
+        {
+            health = 0;
+        }
+        //Print "[player name] has [health] health remaining."
+        System.out.println(name + " has " + health + " health remaining.")
+    }
 
-    Method heal(amount):
-        Add amount to health
-        If health is greater than maxHealth, set health to maxHealth
-        Print "[player name] healed for [amount] health. Now has [health] health."
+    //Method heal(amount):
+    public void heal(int amount)
+    {
+        //Add amount to health
+        health += amount;
+        //If health is greater than maxHealth, set health to maxHealth
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        //Print "[player name] healed for [amount] health. Now has [health] health."
+        System.out.println(name + " healed for " + amount + " health. Now has " + health + " health.");
+    }
 
-   Method addXP(amount):
-        Add amount to xp
-        Print "[player name] gained [amount] XP. Total: [xp]/[xpToNextLevel]"
-        If xp is greater than or equal to xpToNextLevel,
-            Call levelUp()
-            Return true
-        Return false
+   //Method addXP(amount):
+    public boolean addXp(int amount)
+    {
+        //Add amount to xp
+        xp += amount;
+        //Print "[player name] gained [amount] XP. Total: [xp]/[xpToNextLevel]"
+        System.out.println(name + " gained " + amount + " XP. Total: " + xp + "/" + xptoNextLevel);
+        //If xp is greater than or equal to xpToNextLevel,
+        if (xp >= xpToNextLevel)
+        {
+            //Call levelUp()
+            return levelUp();
+            //Return true
+        }
+        //Return false
+        return false;
+    }
 
-    Method levelUp():
-        Increment level
-        Increase maxHealth by 20
-        Set health to maxHealth
-        Increase attack by 5
-        Set xp to 0
-        Print "[player name] leveled up to level [level]!"
-        Print "Health increased to [maxHealth]"
-        Print "Attack increased to [attack]"
-        Return true
+    //Method levelUp():
+    public boolean levelUp()
+    {
+        //Increment level
+        level++;
+        //Increase maxHealth by 20
+        maxHealth += 20;
+        //Set health to maxHealth
+        health = maxHealth;
+        //Increase attack by 5
+        attack += 5;
+        //Set xp to 0
+        xp = 0;
+        //Print "[player name] leveled up to level [level]!"
+        System.out.println(name + " leveled up to level " + level + "!");
+        //Print "Health increased to [maxHealth]"
+        System.out.println("Health increased to " + maxHealth);
+        //Print "Attack increased to [attack]"
+        System.out.println("Attack increased to " + attack);
+        //Return true
+        return true;
+    }
 
-    Method addPowerUp(powerUp):
-        Add powerUp to powerUps list
-        Print "[player name] gained the [powerUp] power-up!"
+    //Method addPowerUp(powerUp):
+    public void addPowerUp(String powerUp)
+    {
+        //Add powerUp to powerUps list
+        powerUps.add(powerUp);
+        //Print "[player name] gained the [powerUp] power-up!"
+        System.out.println(name + " gained the " powerUp + " power-up!");
+    }
 
-   Method usePowerUp(enemy, powerUpIndex):
-        If powerUpIndex is valid (within bounds of powerUps list):
-            powerUp = Remove powerUp from powerUps list at powerUpIndex
+   //Method usePowerUp(enemy, powerUpIndex):
+    public void usePowerUp(Enemy enemy, int powerUpIndex)
+    {
+        //If powerUpIndex is valid
+        if (powerUpIndex >= 0 && powerUpIndex < powerUps.size())
+        {
+            //powerUp = Remove powerUp from powerUps list at powerUpIndex
+            String powerUp = powerUps.remove(powerUpIndex);
 
-            If powerUp is "Double Attack":
-                Print "[player name] uses Double Attack!"
-                Call attackEnemy(enemy)
-                Call attackEnemy(enemy)
-            Else If powerUp is "Heal":
-                Print "[player name] uses Heal!"
-                Call heal(30)
-            Else If powerUp is "Shield":
-                Print "[player name] uses Shield for the next attack!"
-                // Shield implementation would be handled in the battle logic, likely in the Game class
-        Else:
-            Print "Invalid power-up index."
+            //If powerUp is "Double Attack":
+            if (powerUp.equals("Double Attack"))
+            {
+                //Print "[player name] uses Double Attack!"
+                System.out.println(name + " uses Double Attack!");
+                //Call attackEnemy(enemy)
+                attackEnemy(enemy);
+                //Call attackEnemy(enemy)
+                attackEnemy(enemy);
+            }
+            //Else If powerUp is "Heal":
+            else if (powerUp.equals("Heal"))
+            {
+                //Print "[player name] uses Heal!"
+                System.out.println(name + " uses Heal!");
+                //Call heal(30)
+                heal(30);
+            }
+            //Else If powerUp is "Shield":
+            else if (powerUp.equals("Shield"))
+            {
+                //Print "[player name] uses Shield for the next attack!"
+                System.out.println(name + " uses Shield for the next attack!");
+                // Shield implementation is in the game class
+            }
+        }
+        //Else:
+        else
+        {
+            //Print "Invalid power-up index."
+            System.out.println("Invalid power-up index.");
+        }
+    }
 
-    Method displayStats():
-        Print "Name: " + name
-        Print "Class: " + characterClass
-        Print "Level: " + level
-        Print "Health: " + health + "/" + maxHealth
-        Print "Attack: " + attack
-        Print "XP: " + xp + "/" + xpToNextLevel
-        Print "Power-ups: " + powerUps
+    //Method displayStats():
+        //Print "Name: " + name
+        //Print "Class: " + characterClass
+        //Print "Level: " + level
+        //Print "Health: " + health + "/" + maxHealth
+        //Print "Attack: " + attack
+        //Print "XP: " + xp + "/" + xpToNextLevel
+        //Print "Power-ups: " + powerUps
 
-    Method getName():
-        Return name
+    //Method getName():
+        //Return name
 
-    Method getCharacterClass():
-        Return characterClass
+    //Method getCharacterClass():
+        //Return characterClass
 
-    Method getHealth():
-        Return health
+    //Method getHealth():
+        //Return health
 
-    Method getMaxHealth():
-        Return maxHealth
+    //Method getMaxHealth():
+        //Return maxHealth
 
-    Method getAttack():
-        Return attack
+    //Method getAttack():
+        //Return attack
 
-    Method getLevel():
-        Return level
+    //Method getLevel():
+        //Return level
 
-    Method getXp():
-        Return xp
+    //Method getXp():
+        //Return xp
 
-    Method getXpToNextLevel():
-        Return xpToNextLevel
+    //Method getXpToNextLevel():
+        //Return xpToNextLevel
 
-    Method getPowerUps():
-        Return powerUps
+    //Method getPowerUps():
+        //Return powerUps
 
 
 
